4394a02f1b9dfd326866a1a2f756b828565c2890
Isha-odoo <isha3031998@gmail.com>
2026-04-22 17:16:04 +0530

    Implement login function using Supabase

---
+++ login.js
@@ -0,0 +1,24 @@
+const SUPABASE_URL = "https://styskdmjtfbbbrjjkfxe.supabase.co";
+const SUPABASE_KEY = "sb_publishable_1IvvlwE_CVoFzV8R6LkSnA_nATdDE7l";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+async function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const name = document.getElementById("name").value;
+
+  const { error } = await supabaseClient.auth.signInWithPassword({
+    email,
+    password
+  });
+
+  if (error) {
+    alert(error.message);
+    return;
+  }
+
+  localStorage.setItem("chat_name", name);
+
+  window.location.href = "chat.html";
+}
